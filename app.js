a878437768c5269a82dfc36ad9b5396bcf46f5f4
jordan-storz <jordan.storz.1@gmail.com>
2016-12-16 15:11:10 -0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var userRoutes = require('./routes/user')
+var cors = require('cors');
+app.use(cors());
 app.use('/user', userRoutes)
 
 app.listen(process.env.PORT || 3000, function() { //app.listen takes two arguments like here, a port number and a function
