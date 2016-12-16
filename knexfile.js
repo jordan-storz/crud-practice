7e2c7adf04f60c8edd0ea3663151322eca6c7f42
“lukemccrae” <lukemccrae@gmail.com>
2016-12-16 11:04:18 -0700

    commit after heroku push

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,15 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
     development: {
         client: 'postgresql',
         connection: 'postgres://localhost/sticker-app'
+    },
+
+    production: {
+        client: 'postgresql',
+        connection: process.env.DATABASE_URL + '?ssl=true'
     }
 };
